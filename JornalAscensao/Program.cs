eb2eca742688a6afc1e2072d93248328f8c86bf5
Nicolas Teofilo <92840990+GalderioJaguara@users.noreply.github.com>
2025-09-01 18:58:53 -0300

    Registro das injeções de dependência

--- JornalAscensao/Program.cs
+++ JornalAscensao/Program.cs
@@ -1,7 +1,13 @@
+using JornalAscensao.Data.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services
+    .AdicionarBancoDeDados(builder.Configuration)
+    .AdicionarIdentity();
 
 var app = builder.Build();
 
