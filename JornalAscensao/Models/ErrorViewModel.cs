75be48b671d3c37732baffbf2a4ca013decb683d
Nicolas Teofilo <92840990+GalderioJaguara@users.noreply.github.com>
2025-09-01 18:09:54 -0300

    first commit

---
+++ JornalAscensao/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JornalAscensao.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
