761fb79870930f1ad68ebb0503f66d4a2e60627e
Nicolas Teofilo <92840990+GalderioJaguara@users.noreply.github.com>
2025-09-01 18:55:23 -0300

    adição da entidade Usuario

--- JornalAscensao/Models/Usuario.cs
+++ JornalAscensao/Models/Usuario.cs
@@ -1,6 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
 namespace JornalAscensao.Models;
 
-public class Usuario
+public class Usuario : IdentityUser
 {
-    
+    [Required, StringLength(50, MinimumLength = 3)]
+    public string Apelido { get; set; } = String.Empty;
+    public string? Avatar { get; set; }
 }
