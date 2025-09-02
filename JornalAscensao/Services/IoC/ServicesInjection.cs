82d9d8d5e3b5c24ed7c2cee5040a1cb3de57a902
Nicolas Teofilo <92840990+GalderioJaguara@users.noreply.github.com>
2025-09-02 15:31:16 -0300

    Adição dos serviços de autenticação a injeção de dependências

--- JornalAscensao/Services/IoC/ServicesInjection.cs
+++ JornalAscensao/Services/IoC/ServicesInjection.cs
@@ -1,9 +1,12 @@
+using JornalAscensao.Services.Abstraction;
+
 namespace JornalAscensao.Services.IoC;
 
 public static class ServicesInjection
 {
     public static IServiceCollection AdicionarServicos(this IServiceCollection services)
     {
+        services.AddScoped<IAutenticacaoService, AutenticacaoService>();
         return services;
     }
 }
