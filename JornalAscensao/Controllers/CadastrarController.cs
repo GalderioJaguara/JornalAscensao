f7bff9df2a8040e7a7412aab1de4d6c7a8eb8bd5
Nicolas Teofilo <92840990+GalderioJaguara@users.noreply.github.com>
2025-09-02 16:40:24 -0300

    Página de Cadastro

--- JornalAscensao/Controllers/CadastrarController.cs
+++ JornalAscensao/Controllers/CadastrarController.cs
@@ -1,6 +1,28 @@
+using JornalAscensao.Models;
+using JornalAscensao.Services.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+
 namespace JornalAscensao.Controllers;
 
-public class CadastrarController
+public class CadastrarController(IAutenticacaoService autenticacaoService) : Controller
 {
-    
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Index([Bind("Apelido, Email, Senha, ConfirmarSenha")] CadastrarViewModel usuario)
+    {
+        if (ModelState.IsValid)
+        {
+            var usuarioCadastrado = await autenticacaoService.CadastrarUsuarioAsync(usuario);
+            if (usuarioCadastrado.Succeeded) 
+                return Redirect($"Home/Index/");
+                
+        }
+        Console.WriteLine(await autenticacaoService.CadastrarUsuarioAsync(usuario));
+        return View(usuario);
+    }
 }
