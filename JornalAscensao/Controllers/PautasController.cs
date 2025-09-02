cb82cd427821781f66ad9a0470ff13e7327ee452
Nicolas Teofilo <92840990+GalderioJaguara@users.noreply.github.com>
2025-09-02 18:53:12 -0300

    Criação da página principal das pautas, página de criação e edição de pautas

---
+++ JornalAscensao/Controllers/PautasController.cs
@@ -0,0 +1,92 @@
+using JornalAscensao.Models;
+using JornalAscensao.Services.Abstraction;
+using JornalAscensao.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JornalAscensao.Controllers;
+
+public class PautasController(IPautaService pautaService) : Controller
+{
+    // GET
+    public async Task<IActionResult> Index()
+    {
+        var pautas = await pautaService.GetPautasAsync();
+        return View(pautas);
+    }
+    
+    public ActionResult Cadastrar()
+    {
+        var ModelComSelects = new PautaFormViewModel();
+        return View(ModelComSelects);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> Cadastrar(PautaFormViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var novaPauta = await pautaService.CriarPautaAsync(model);
+
+            return Redirect($"Index");
+        }
+            
+        return View(model);
+    }
+    
+    public async Task<ActionResult> Editar(Guid id)
+    {
+        var pauta = await pautaService.GetPautaAsync(id);
+        if (pauta == null)
+        {
+            TempData["error"] = "Pauta não encontrada";
+            return RedirectToAction("Index");
+        }
+            
+        var pautaFormViewModel = new PautaFormViewModel
+        {
+            Titulo = pauta.Titulo,
+            Categoria = pauta.Categoria,
+            Descricao = pauta.Descricao,
+            Categorias = PautaUtils.GetCategoriasSelectList(),
+            Imagem = pauta.Imagem,
+            LinkConteudo = pauta.LinkConteudo,
+            Tipo = pauta.Tipo,
+            Tipos = PautaUtils.GetTipoSelectList()
+        };
+        return View(pautaFormViewModel);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> Editar(Guid id, PautaFormViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+        var pautaAtualizada = await pautaService.AtualizarPautaAsync(id, model);
+
+        if (pautaAtualizada == null)
+        {
+            TempData["error"] = "Pauta não Encontrada";
+        }
+        return RedirectToAction("Index");
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetLinkConteudoInformacao([FromQuery]string url)
+    {
+        var info = await pautaService.GetPautaMetadadosAsync(url);
+        return Ok(info);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetConteudoPauta([FromQuery] Guid id)
+    {
+        var pauta = await pautaService.GetPautaAsync(id);
+        if (pauta == null)
+            return NotFound();
+        return  Ok(pauta);
+    } 
+}
