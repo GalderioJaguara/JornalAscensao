319641b1cf929f2d0439be9a17908466606535ed
Nicolas Teofilo <92840990+GalderioJaguara@users.noreply.github.com>
2025-09-11 21:04:29 -0300

    Adição de migrations para suporte a timestamps em usuários e slug único para artigos.

---
+++ JornalAscensao/Migrations/20250909130143_ArtigoSlug.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JornalAscensao.Migrations
+{
+    /// <inheritdoc />
+    public partial class ArtigoSlug : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Artigos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Artigos");
+        }
+    }
+}
---
+++ JornalAscensao/Migrations/20250911214928_SlugUnique.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JornalAscensao.Migrations
+{
+    /// <inheritdoc />
+    public partial class SlugUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                table: "Artigos",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artigos_Slug",
+                table: "Artigos",
+                column: "Slug",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Artigos_Slug",
+                table: "Artigos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Slug",
+                table: "Artigos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
